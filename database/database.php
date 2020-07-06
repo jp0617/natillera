fe3c253e2e941b3fe9b34a426c5af0a7b8305c02
jp0617 <juan.ramirez6323@unaula.edu.co>
2020-07-06 12:36:19 -0500

    conexion a la base de datos

---
+++ database/database.php
@@ -0,0 +1,20 @@
+<?php
+class database{
+    public static function getconnection(){
+        $conn= new mysqli('localhost','root','qQ12345678','dbNatillera');
+        $conn->query("set names 'utf8'");
+        if ($conn->connect_error) {
+            die('no se pudo conectar a la base de dato' . $conn->connect_error);
+        } else {
+            return $conn;
+        }
+    }
+
+    public function protect($v){
+        $conn= new mysqli('localhost','root','qQ12345678','dbNatillera');
+        $v=mysqli_real_escape_string($conn,$v);
+        $v=htmlentities($v,ENT_QUOTES);
+        $v=trim($v);
+        return $v;
+    }
+}
