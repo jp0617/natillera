95eaa1469953e3ecbe1486c0dc024a9e92531126
jp0617 <juan.ramirez6323@unaula.edu.co>
2020-07-06 16:25:59 -0500

    prueba

---
+++ controller/core.php
@@ -0,0 +1,9 @@
+<?php
+//******nucleo*****////
+define('HTML_DIR', 'view/html/'); //DIRECION DE LA VISTA
+define('HTML_DIR_BASIC', 'view/html_basico/'); //DIRECION DE LA VISTA BASICO
+define('APP_TITTLE', 'natillera'); //TITULO
+define('APP_HOST', 'localhost');
+define('APP_USER', 'root');
+define('APP_PASS', '');
+define('APP_DB', '');
