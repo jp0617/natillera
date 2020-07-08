fce315806bea04fd794f6867b7ba3cb29adc224d
jp0617 <juan.ramirez6323@unaula.edu.co>
2020-07-08 16:06:51 -0500

    Se agrego funcion index y core

--- index.php
+++ index.php
@@ -2,6 +2,23 @@
 session_start();
 require 'database/database.php';
 require 'controller/core.php';
-require 'view/html/index.html'
+
+function index($controller)
+{
+    require_once "controller/$controller" . ".controller.php";
+    $controller = ucwords($controller);
+    $controller = new $controller;
+    $controller->index();
+}
+
+if (isset($_POST['ema']) && isset($_POST['pas'])) {
+    $login = "login";
+    require_once "controller/$login" . ".controller.php";
+    $login = ucwords($login) . '.controller';
+    $login = new $login;
+    $login->validacion();
+} else if (!isset($_SESSION['ema'])) {
+    index("login");
+}
 
 ?>
