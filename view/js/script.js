8472c81423819d56789f577b6ef6db74e63c360e
jp0617 <juan.ramirez6323@unaula.edu.co>
2020-07-09 13:37:36 -0500

    Se implemento el sweetalert

--- view/js/script.js
+++ view/js/script.js
@@ -1,5 +1,13 @@
-function login(type_error) {
-  if (type_error == "Email") {
-    alert("Verifique su email");
+function Login(type_error) {
+  if (type_error == "email") {
+    Swal.fire({
+      icon: "error",
+      title: "Lo sentimos",
+      text: "No es valido",
+    });
+  } else if (type_error == "password") {
+    var men = document.getElementById("messageWrong");
+    men.innerHTML =
+      "Contraseña incorrecta, por favor verifique la contraseña o póngase en contacto con nuestro administrador";
   }
 }
