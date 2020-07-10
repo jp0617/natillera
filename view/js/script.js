2e56dde1098fe88755ff6f706d7579dfd8b50dab
jp0617 <juan.ramirez6323@unaula.edu.co>
2020-07-10 17:34:58 -0500

    se integro sweetalert

--- view/js/script.js
+++ view/js/script.js
@@ -1,13 +1,36 @@
 function Login(type_error) {
   if (type_error == "email") {
+    
     Swal.fire({
       icon: "error",
       title: "Lo sentimos",
       text: "No es valido",
     });
   } else if (type_error == "password") {
-    var men = document.getElementById("messageWrong");
-    men.innerHTML =
-      "Contraseña incorrecta, por favor verifique la contraseña o póngase en contacto con nuestro administrador";
+    
+    Swal.fire({
+      icon: "error",
+      title: "Lo sentimos",
+      text: "Por favor revisa tu contraseña",
+    });
+  } else if (type_error == "success") {
+    
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      onOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+    
+    Toast.fire({
+      icon: 'success',
+      title: 'bienvenido'
+
+    })
   }
 }
