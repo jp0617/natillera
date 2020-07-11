3b3ea4b4b30e63973a7dff0f06525ff1875e77af
jp0617 <juan.ramirez6323@unaula.edu.co>
2020-07-11 15:12:00 -0500

    mensaje post login

--- view/js/script.js
+++ view/js/script.js
@@ -1,36 +1,36 @@
 function Login(type_error) {
   if (type_error == "email") {
-    
     Swal.fire({
       icon: "error",
       title: "Lo sentimos",
       text: "No es valido",
     });
   } else if (type_error == "password") {
-    
     Swal.fire({
       icon: "error",
       title: "Lo sentimos",
       text: "Por favor revisa tu contraseña",
     });
-  } else if (type_error == "success") {
-    
-    const Toast = Swal.mixin({
-      toast: true,
-      position: 'top-end',
-      showConfirmButton: false,
-      timer: 3000,
-      timerProgressBar: true,
-      onOpen: (toast) => {
-        toast.addEventListener('mouseenter', Swal.stopTimer)
-        toast.addEventListener('mouseleave', Swal.resumeTimer)
-      }
-    })
-    
-    Toast.fire({
-      icon: 'success',
-      title: 'bienvenido'
-
-    })
   }
 }
+
+function Loggedin(user) {
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    onOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
+
+  Toast.fire({
+    icon: "success",
+    title: "Bienvenido " + user,
+  });
+}
+
+
