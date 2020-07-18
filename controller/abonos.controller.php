d7c778318f2904c74b2a78129170819e490e9fad
jp0617 <juan.ramirez6323@unaula.edu.co>
2020-07-17 19:48:25 -0500

    página y controlador de abonos

---
+++ controller/abonos.controller.php
@@ -0,0 +1,10 @@
+<?php
+class abonos{
+    public function index(){
+        $tittle="Abonos";
+        require_once HTML_DIR ."overall/header.html";
+        require_once HTML_DIR."admin/home.html";
+        require_once HTML_DIR."abonos/abonos.html";
+        require_once HTML_DIR."overall/footer.html";
+    }
+}
