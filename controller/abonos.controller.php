d4cae59d9d556885af4e50270989de55fa63f9ec
ampv <angelica.piedrahita8569@unaula.edu.co>
2020-08-06 11:07:25 -0500

    actualizacion

--- controller/abonos.controller.php
+++ controller/abonos.controller.php
@@ -3,7 +3,7 @@ class abonos{
     public function index(){
         $tittle="Abonos";
         require_once HTML_DIR ."overall/header.html";
-        require_once HTML_DIR."admin/home.html";
+        require_once HTML_DIR."admin/home.html"; 
         require_once HTML_DIR."abonos/abonos.html";
         require_once HTML_DIR."overall/footer.html";
     }
